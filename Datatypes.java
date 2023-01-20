cb8de5892f2b932d716a08a1e8b021bc24521519
vamsi12145 <polepallebharathwaj8586@gmail.com>
2023-01-20 22:22:04 +0530

    my first java

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+class Hello
+{
+    public static void  main(String a[])
+    {
+        int num=3;
+        //int num1=5;
+        //int result=num+num1;
+        byte b=127; //for byte it takes -128 to +128
+        short s=558;
+        float f=2.3f;
+        double d=2.8;
+        long l=33443l;
+        char c='k';
+        boolean y =true;
+        //System.out.println(result);
+    }
+}
